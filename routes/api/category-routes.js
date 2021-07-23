b7c53e98c27e0a23d7a0b9ff6b3c823ac59209f1
Andrew <wing_andrew@hotmail.com>
2021-07-22 21:57:49 -0400

    Troubleshooting category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,3 +1,6 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
 // The `/api/categories` endpoint
 
 // find all categories and associated Products
