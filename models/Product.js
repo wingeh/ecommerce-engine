2a38492a6e73289a32dfb4bc4a7fc969be699415
Andrew <wing_andrew@hotmail.com>
2021-07-23 22:43:04 -0400

    Correcting Product model

--- models/Product.js
+++ models/Product.js
@@ -6,49 +6,47 @@ const sequelize = require('../config/connection');
 // Initialize Product model by extending Sequelize's Model class
 class Product extends Model {}
 
-// Fields and rules for Product model
+// set up fields and rules for Product model
 Product.init(
   {
+    // define columns
     id: {
-      type: DataTypes.INTEGER,
+      type:DataTypes.INTEGER,
       allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+      primaryKey:true,
+      autoIncrement:true 
     },
-    product_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+    product_name: { 
+      type:DataTypes.STRING,
+      allowNull: false
+  },
+  
+    price: { 
+      type:DataTypes.DECIMAL, 
+      allowNull:false,
+      validate:{ 
+        isDecimal:true,
+      },
     },
-    price: {
-      type: DataTypes.DECIMAL(10, 2),
-      allowNull: false,
-      validate: {
-        isDecimal: true
-      }
-    },
-    stock: {
-      type: DataTypes.INTEGER,
-      defaultValue: 10,
-      allowNull: false,
-      validate: {
-        isNumeric: true
-      }
-    },
-    category_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'category',
-        id: 'id'
-      }
+
+    Stock: {
+      type: DataTypes.INTEGER, 
+      allowNull:false, 
+      defaultValue:10,
+      validate:{
+        isNumeric:true,
+      },
     }
   },
-  {
+  
+  { 
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: 'product', 
   }
+
 );
 
 module.exports = Product;
